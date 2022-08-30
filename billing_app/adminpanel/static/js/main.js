f4dc7f5a1183e22880800a95c8052a973a97db27
Ambareen09 <ambubushra@gmail.com>
2022-08-30 17:02:40 +0530

    templates added

---
+++ billing_app/adminpanel/static/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+
+	"use strict";
+
+	 $(document).ready(function() {
+        $('#multiple-checkboxes,#type-checkboxes').multiselect({
+          includeSelectAllOption: true,
+        });
+    });
+	 
+})(jQuery);
