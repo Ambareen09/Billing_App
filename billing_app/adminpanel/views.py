abb8f7db4a9a2d07f4c2afcd21c065bfbfdf13d1
Ambareen09 <ambubushra@gmail.com>
2022-09-01 00:14:47 +0530

    pages linked

--- billing_app/adminpanel/views.py
+++ billing_app/adminpanel/views.py
@@ -11,3 +11,48 @@ def index(request):
 class InventoryView(View):
     def get(self, request):
         return render(request, "adminpanel/viewinventory.html")
+
+
+class AddInventoryView(View):
+    def get(self, request):
+        return render(request, "adminpanel/addinventory.html")
+
+
+class AddBillingView(View):
+    def get(self, request):
+        return render(request, "adminpanel/addbilling.html")
+
+
+class BillingHistoryView(View):
+    def get(self, request):
+        return render(request, "adminpanel/billinghistory.html")
+
+
+class ExpenseView(View):
+    def get(self, request):
+        return render(request, "adminpanel/expense.html")
+
+
+class SalaryView(View):
+    def get(self, request):
+        return render(request, "adminpanel/salary.html")
+
+
+class PendingBillsView(View):
+    def get(self, request):
+        return render(request, "adminpanel/pendingbills.html")
+
+
+class VendorView(View):
+    def get(self, request):
+        return render(request, "adminpanel/vendor.html")
+
+
+class StaffView(View):
+    def get(self, request):
+        return render(request, "adminpanel/staff.html")
+
+
+class CustomerView(View):
+    def get(self, request):
+        return render(request, "adminpanel/customer.html")
